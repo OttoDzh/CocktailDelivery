34ccbfd70133e1eeaff34507b909ce1a60f8b5f6
Otto <jiokotok@yandex.ru>
2023-07-07 23:40:05 +0300

    Initial Commit

---
+++ CocktailDelivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocktailDelivery
+//
+//  Created by Otto Dzhandzhuliya on 07.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
